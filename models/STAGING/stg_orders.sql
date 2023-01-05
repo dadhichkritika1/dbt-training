a5fff5e42ff943a7c262ed64dce28c833ed3db24
kritika dadhich <kritikadadhich00@gmail.com>
2023-01-05 11:08:27 +0000

    developing stg_order

---
+++ models/STAGING/stg_orders.sql
@@ -0,0 +1,12 @@
+select o.orderid,o.orderdate, o.shipdate ,o.shipmode
+ ,o.ordersellingprice - o.ordercostprice as ordersprofit,
+ o.ordercostprice,o.ordersellingprice,
+ c.customername,c.segment,c.country,
+ p.category, p.productname,p.subcategory
+ from {{ ref('raw_orders') }} as o
+ left join 
+ {{ ref('raw_customer') }} as c
+ on o.CUSTOMRTID= c.customerid
+ left join
+ {{ ref('raw_product') }} as p
+ on o.productid=p.productid
